66b5a81b94822dd056fab679528700c19050ab5d
JKalonewolf <jayakumarkumar7783@gmail.com>
2024-12-13 06:09:09 -0500

    Adding temporary car mode and car make

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
 'https://u2414104443-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://u2414104443-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://u2414104443-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
