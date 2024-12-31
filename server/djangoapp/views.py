d6038d7c22e6eb5aeccbd69d10daf0d343673800
JKalonewolf <jayakumarkumar778@gmail.com>
2024-12-31 00:07:04 -0500

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -62,18 +62,35 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
+    context = {}
+
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
-    if User.objects.filter(username=username).exists():
-        logger.info(f"Registration attempt with existing username: {username}")
-        return JsonResponse({"userName": username, "error": "Already Registered"})
-    user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
-    login(request, user)
-    return JsonResponse({"userName": username, "status": "Authenticated"})
+    username_exist = False
+    email_exist = False
+    try:
+        # Check if user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except:
+        # If not, simply log this is a new user
+        logger.debug("{} is new user".format(username))
+
+    # If it is a new user
+    if not username_exist:
+        # Create user in auth_user table
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        # Login the user and redirect to list page
+        login(request, user)
+        data = {"userName":username,"status":"Authenticated"}
+        return JsonResponse(data)
+    else :
+        data = {"userName":username,"error":"Already Registered"}
+        return JsonResponse(data)
 
 def get_cars(request):
     if CarMake.objects.count() == 0:
